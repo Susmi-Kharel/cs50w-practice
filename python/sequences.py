b92ec586d4b31480a95b90d9d2f843ebd0b4575c
Susmi Kharel <susmi.kharel@gmail.com>
2025-07-21 13:10:25 -0400

    new py files

---
+++ python/sequences.py
@@ -0,0 +1,15 @@
+# List - sequence of mutable values
+name = ["Harry","Ron","Hermione"]
+print(name[0])
+
+# Tuples - sequence of immutable values
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0,20.0)
+
+#set - collection of unique values
+
+
+#dict - collection of key-value pairs
+
